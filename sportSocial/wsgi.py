b60785e7f4e49ee5b757b8df57504a52421f89bb
ingthorb <ingthorb@gmail.com>
2019-10-03 14:40:48 +0200

    Starting Heroku and Jenkins plugin

--- sportSocial/wsgi.py
+++ sportSocial/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for sportsMeetup project.
+WSGI config for sportSocial project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sportsMeetup.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sportSocial.settings')
 
 application = get_wsgi_application()
