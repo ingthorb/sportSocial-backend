c4640a2bb851c1d19729aefdb246521a6e3ba170
ingthorb <ingthorb@gmail.com>
2019-10-07 15:09:17 +0200

    Added Tests

--- sportSocialBackend/API/tests.py
+++ sportSocialBackend/API/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
-
+from .models import User, Country
 # Create your tests here.
+
+
+class UserModelTests(TestCase):
+    def setUp(self):
+        country = Country.objects.create(name="test")
+        User.objects.create(username='ingthor', country=country, age=10)
+
+    def test_created_user(self):
+        user = User.objects.get(username='ingthor')
+        self.assertEqual(user.username, 'ingthor')
