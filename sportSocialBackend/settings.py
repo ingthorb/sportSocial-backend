5bb07fe40a3bd1ff23a154143a71f7e98a6c9903
ingthorb <ingthorb@gmail.com>
2021-01-24 18:01:17 +0100

    Update settings.py

--- sportSocialBackend/settings.py
+++ sportSocialBackend/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 VENV_PATH = os.path.dirname(BASE_DIR)
-STATIC_ROOT = os.path.join(VENV_PATH, 'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
 # Activate Django-Heroku
 django_heroku.settings(locals())
