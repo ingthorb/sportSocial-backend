37942bc8b4964ee9e1efcf2967e1f62029bf9e20
ingthorb <ingthorb@gmail.com>
2021-03-09 22:49:49 +0100

    Update the tests
    Set the standard for tests

--- API/tests.py
+++ API/tests.py
@@ -1,12 +1,36 @@
 from rest_framework.test import APITestCase
-from API.models import User, Country
-from API.serializers import UserSerializer
+from API.models import Sport, Country, City
 
-class UserModelTests(APITestCase):
+
+class SportViewTestCase(APITestCase):
+
     def setUp(self):
-        self.country = Country.objects.create(name='Switzerland')
-        User.objects.create(username='ingthor', age=10, country=self.country)
+        self.sport_name = 'Football'
+        self.description = 'Play with your feet'
 
-    def test_created_user(self):
-        user = User.objects.get(username='ingthor')
-        self.assertEqual(user.username, 'ingthor')
+    def test_create_sport(self):
+        """
+            Ensure we can create a new Sport object.
+        """
+        data = {'name': self.sport_name,
+                'description': self.description }
+        response = self.client.post("/sports/", data, format='json')
+        self.assertEqual(201, response.status_code)
+        self.assertEqual(Sport.objects.count(), 1)
+        self.assertEqual(Sport.objects.get().name, self.sport_name)
+
+
+class CountryViewTestCase(APITestCase):
+
+    def setUp(self):
+        self.country_name = 'Switzerland'
+
+    def test_create_country(self):
+        """
+            Ensure we can create a new country object.
+        """
+        data = {'name': self.country_name}
+        response = self.client.post("/countries/", data, format='json')
+        self.assertEqual(201, response.status_code)
+        self.assertEqual(Country.objects.count(), 1)
+        self.assertEqual(Country.objects.get().name, self.country_name)
