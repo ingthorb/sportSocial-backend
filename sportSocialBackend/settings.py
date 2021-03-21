3e5430f40d7c8c0b23d8ef8e7d47a9930dec7afe
ingthorb <ingthorb@gmail.com>
2021-03-21 14:17:09 +0100

    Update settings.py

--- sportSocialBackend/settings.py
+++ sportSocialBackend/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:8080",
-    "http://staging.sportsocial.is"
+    "http://staging.sportsocial.is",
     "http://sportsocial.is",
     "http://127.0.0.1:3000"
 ]
