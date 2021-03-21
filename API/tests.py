934398e8628537f5e303657059d619ceb13dc21f
ingthorb <ingthorb@gmail.com>
2021-03-21 14:11:46 +0100

    Fix CORS issues for frontend

--- API/tests.py
+++ API/tests.py
@@ -34,3 +34,21 @@ class CountryViewTestCase(APITestCase):
         self.assertEqual(201, response.status_code)
         self.assertEqual(Country.objects.count(), 1)
         self.assertEqual(Country.objects.get().name, self.country_name)
+
+
+class CitiesViewTestCase(APITestCase):
+
+    def setUp(self):
+        self.city_name = 'Bern'
+        self.country_name = 'Switzerland'
+        self.country = Country.objects.create(name=self.country_name)
+
+    def test_create_city(self):
+        """
+            Ensure we can create a new city object.
+        """
+        data = {'name': self.city_name, 'country': self.country.id}
+        response = self.client.post("/cities/", data, format='json')
+        self.assertEqual(201, response.status_code)
+        self.assertEqual(City.objects.count(), 1)
+        self.assertEqual(City.objects.get().name, self.country_name)
--- sportSocialBackend/settings.py
+++ sportSocialBackend/settings.py
@@ -28,7 +28,8 @@ DEBUG = True
 
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:8080",
-    "http://localhost:3000",
+    "http://staging.sportsocial.is"
+    "http://sportsocial.is",
     "http://127.0.0.1:3000"
 ]
 
